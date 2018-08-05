4a2906377ff88f82fdc9b84d4a7e5cd76b23e228
shailesh <shailesh.vanaliya91@gmail.com>
2018-08-05 10:38:43 +0530

    expnand left panel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,21 @@
 
     @include('layouts.include.header')
     <body class="o-page ">
-        
+        @php
+        $session = Session::all();
+        @endphp
         @include('layouts.include.breadcrumb')
         @include('layouts.include.message')
-        @include('layouts.include.left-sidebar')
+        
+        @if($session['logindata'][0]['type'] == 2)
+            @include('layouts.include.leftpanel.admin-left-sidebar')
+        @elseif($session['logindata'][0]['type'] == 1)
+            @include('layouts.include.leftpanel.custome-left-sidebar')
+        @elseif($session['logindata'][0]['type'] == 0)
+            @include('layouts.include.leftpanel.user-left-sidebar')
+        @else
+        
+        @endif
         
     <main class="o-page__content">
         @yield('content')
---
+++ resources/views/layouts/include/leftpanel/user-left-sidebar.blade.php
@@ -0,0 +1,20 @@
+<div class="o-page__sidebar js-page-sidebar">
+    <div class="c-sidebar">
+        <a class="c-sidebar__brand" href="{{ route('user-dashboard') }}">
+            <img class="c-sidebar__brand-img" src="img/logo.png" alt="Logo"> Dashboard
+        </a>
+        <h4 class="c-sidebar__title">Dashboards</h4>
+        <ul class="c-sidebar__list">
+            <li class="c-sidebar__item">
+                <a class="c-sidebar__link is-active" href="{{ route('user-dashboard') }}">
+                    <i class="fa fa-home u-mr-xsmall"></i>Overview
+                </a>
+            </li>
+            <li class="c-sidebar__item">
+                <a class="c-sidebar__link" href="javascript:;">
+                    <i class="fa fa-user-o u-mr-xsmall"></i>Manage User
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
