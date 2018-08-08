c2ac10dbf90ce0f9a60b5ead8874cef217e7f444
shailesh <shailesh.vanaliya91@gmail.com>
2018-08-08 23:21:18 +0530

    changee remov type from edit user

--- app/Model/Users.php
+++ app/Model/Users.php
@@ -13,14 +13,10 @@ class Users extends Model {
 
     public function gtUsrLlist($id = NULL) {
         if($id){
-            $result = Users::select('users.*')
-                        ->where('users.id', '=', $id)
-                        ->get();
-            
+            $result = Users::select('users.*')->where('users.id', '=', $id)->get();
         }else{
             $result = Users::get();
         }
-        
         return $result;
     }
 
@@ -31,7 +27,7 @@ class Users extends Model {
         $objUser = new Users();
         $objUser->name = $request->input('first_name');
         $objUser->email = $request->input('email');
-        $objUser->type = '0';
+        $objUser->type = 'USER';
 //        $objUser->role_type = $request->input('role_type');
         $objUser->password = $newpass;
         $objUser->created_at = date('Y-m-d H:i:s');
@@ -45,10 +41,9 @@ class Users extends Model {
         $userId = $request->input('user_id');
         $objUser = Users::find($userId);
         $objUser->name = $request->input('first_name');
-        $objUser->type = '0';
+//        $objUser->type = '0';
         $objUser->updated_at = date('Y-m-d H:i:s');
         $objUser->save();
         return TRUE;
     }
-
 }
