d17cb42c6a99c648890e3f4989ad68a1366074cd
shailesh <shailesh.vanaliya91@gmail.com>
2018-08-08 22:43:15 +0530

    creeate new route file

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -54,6 +54,14 @@ class RouteServiceProvider extends ServiceProvider
         Route::middleware('web')
              ->namespace($this->namespace)
              ->group(base_path('routes/web.php'));
+        
+        Route::middleware('web')
+             ->namespace($this->namespace)
+             ->group(base_path('routes/agent_web.php'));
+        
+        Route::middleware('web')
+             ->namespace($this->namespace)
+             ->group(base_path('routes/customer_web.php'));
     }
 
     /**
