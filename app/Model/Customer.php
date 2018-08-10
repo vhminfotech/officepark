f1fc164389fd5c64ef97292cc34b919d5301cad5
shailesh <shailesh.vanaliya91@gmail.com>
2018-08-10 19:17:16 +0530

    implement new design

---
+++ app/Model/Customer.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
+use DB;
+use Auth;
+
+class Users extends Model {
+
+    protected $table = 'users';
+
+    public function gtUsrLlist($id = NULL) {
+        if($id){
+            $result = Users::select('users.*')->where('users.id', '=', $id)->get();
+        }else{
+            $result = Users::get();
+        }
+        return $result;
+    }
+
+    public function saveUserInfo($request) {
+
+        $newpassword = ($request->input('password') != '') ? $request->input('password') : null;
+        $newpass = Hash::make($newpassword);
+        $objUser = new Users();
+        $objUser->name = $request->input('first_name');
+        $objUser->email = $request->input('email');
+        $objUser->type = 'USER';
+//        $objUser->role_type = $request->input('role_type');
+        $objUser->password = $newpass;
+        $objUser->created_at = date('Y-m-d H:i:s');
+        $objUser->updated_at = date('Y-m-d H:i:s');
+        $objUser->save();
+        return TRUE;
+    }
+
+    public function updateUserInfo($request) {
+        //print_r($request->input('user_id'));
+        $userId = $request->input('user_id');
+        $objUser = Users::find($userId);
+        $objUser->name = $request->input('first_name');
+//        $objUser->type = '0';
+        $objUser->updated_at = date('Y-m-d H:i:s');
+        $objUser->save();
+        return TRUE;
+    }
+}
