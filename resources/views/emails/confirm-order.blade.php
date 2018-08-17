f9f9f5375d112853ab06eb3052f758e5ad01ae3f
abhishekdesai <abhishekdesai39@gmail.com>
2018-08-17 16:28:28 +0530

    Solve Mail Issue

--- resources/views/emails/confirm-order.blade.php
+++ resources/views/emails/confirm-order.blade.php
@@ -3,7 +3,7 @@
     <div style="padding: 48px;">
         <div style="width: 560px; margin: 0 auto; display: block; box-shadow: 0px 0px 14px 0px rgba(142, 140, 140, 0.67); background: #fff; padding: 48px;">
             <!-- wrapper -->
-            <p style=""><img src="<?php echo url('img/logo.gif'); ?>" style="display: block;margin: 0 auto; text-align: center; width: 200px;padding: 10px;"></p>
+            <p style=""><img src="<?php echo url('img/logo.png'); ?>" style="display: block;margin: 0 auto; text-align: center; width: 200px;padding: 10px;"></p>
             <div style="background: linear-gradient(#fff,rgba(222, 13, 0, 0.16)); box-shadow: 0px 0px 16px 0px rgba(0, 0, 0, 0.33); padding: 16px;">
                    <p>Dear {{ $data['interUser'] }}</p>
                    <p>kartik has made clear that they want to close off the listing you have on Look4Muck. Their details are below if you need to contact them first.</p>
@@ -12,7 +12,7 @@
                    <p>If this completes, it is another green shield for your company. Well done.</p>
 
                    <p>Kind Regards</p>
-                   <p>The Look4Muck Team</p>
+                   <p>Office Park Team</p>
                 
             </div>
             <!-- content over -->
