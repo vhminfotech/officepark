dac25c5afbb689bb364f988aced4fb1d3b401e33
abhishekdesai <abhishekdesai39@gmail.com>
2018-08-17 16:58:48 +0530

    Solve mail issue

--- app/Model/Sendmail.php
+++ app/Model/Sendmail.php
@@ -14,26 +14,40 @@ class Sendmail extends Model {
  
    public function sendSMTPMail($mailData)
    {
-       try {
-        $pathToFile = $mailData['attachment'];
-        Mail::send($mailData['template'], ['data' => $mailData['data']], function ($m) use ($mailData,$pathToFile) {
+//       try {
+//        $pathToFile = $mailData['attachment'];
+//        Mail::send($mailData['template'], ['data' => $mailData['data']], function ($m) use ($mailData,$pathToFile) {
+//            $m->from('kartikdesai123@gmail.com', 'Office Park');
+//
+//            $m->to($mailData['mailto'], "Office Park")->subject($mailData['subject']);
+//            if($pathToFile != ""){
+//                $m->attach($pathToFile);
+//            }
+//            
+//           //  $m->cc($mailData['bcc']);
+//        });
+//      }
+//
+//      //catch exception
+//      catch(\Exception $e) {
+//        return true;
+//      }
+      // $pathToFile = $mailData['attachment'];
+       $mailsend = Mail::send($mailData['template'], ['data' => $mailData['data']], function ($m) use ($mailData) {
             $m->from('kartikdesai123@gmail.com', 'Office Park');
 
             $m->to($mailData['mailto'], "Office Park")->subject($mailData['subject']);
-            if($pathToFile != ""){
-                $m->attach($pathToFile);
-            }
+//            if($pathToFile != ""){
+//                $m->attach($pathToFile);
+//            }
             
            //  $m->cc($mailData['bcc']);
         });
-      }
-
-      //catch exception
-      catch(\Exception $e) {
-        return true;
-      }
-       
-        
+        if($mailsend){
+            return true;
+        }else{
+            return false;
+        }
      
    }
     
