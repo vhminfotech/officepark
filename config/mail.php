dc4d4cf77dae8c4feda9dde3bc2d82bc5a0d2b2e
abhishekdesai <abhishekdesai39@gmail.com>
2018-08-17 17:16:34 +0530

    SOlve mail issue

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => 'kartikdesai123@gmail.com',
+        'name' => 'Example',
     ],
 
     /*
