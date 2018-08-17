781f81f1801e60f4b1029eeabe4b19792d8f556c
Chetan <patelchetan1801@gmail.com>
2018-08-18 01:04:08 +0530

    Changes js for validation

--- public/js/admin/updateprofile.js
+++ public/js/admin/updateprofile.js
@@ -2,14 +2,14 @@ var Updateprofile = function () {
 
 
 
-     var handleEditUser = function(){
+    var handleEditUser = function(){
         var form = $('#editUser');
         var rules = {
             name: {required: true},
             inopla_username: {required: true},
             email: {required: true},
             extension_number: {required: true},
-            type: {required: true , number: true},
+            type: {required: true},
             
         };
         handleFormValidate(form, rules, function(form) {
