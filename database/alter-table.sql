7e8fc9337786085cb2fdd40eb2b6241db3ab750a
abhishekdesai <abhishekdesai39@gmail.com>
2018-08-19 13:13:53 +0530

    ALter query

--- database/alter-table.sql
+++ database/alter-table.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `addressbook` ADD `gender` ENUM('M','F') NOT NULL DEFAULT 'M' AFTER `company`, ADD `mobile` INT NULL AFTER `gender`, ADD `telefax` INT NULL AFTER `mobile`, ADD `note` TEXT NULL AFTER `telefax`;
 ALTER TABLE `order_info` ADD `status` ENUM('new','viewed') NOT NULL DEFAULT 'new' AFTER `aggrement`; 
+ALTER TABLE `order_info` ADD `user_id` INT NULL DEFAULT NULL AFTER `id`; 
