ef8c2b6cc713cdee01f780392f050b85366a736b
abhishekdesai <abhishekdesai39@gmail.com>
2018-09-18 03:28:24 +0530

    Solve issue

--- public/js/admin/invoice.js
+++ public/js/admin/invoice.js
@@ -20,6 +20,16 @@ var Invoice = function () {
             var url = baseurl + 'admin/invoice-packege-detail';
             ajaxcall(url,data,function(output){
                 $('.dataAppend').html(output);
+                var finalTotal = 0;
+               
+                $(".Rowtotal").each(function(){
+                    var value = $(this).val();
+                 
+                    if(value != ''){
+                        finalTotal += parseFloat(value);
+                    }
+                });
+                $(".finalTotal").text(finalTotal);
             });
         });
     }
@@ -28,27 +38,25 @@ var Invoice = function () {
         $('body').on('click','.add_new_row',function(){
             
             var option = '<option value="">Select Bezeichnung</option>';
-            var dynamicName = JSON.parse(bezeichnung);
-            var j=0;
             
-            $.each(dynamicName, function(index, value) {
-                option += '<option value=' + index + '>' + value + '</option>';
-            });
+            var j=0;
               
-            var html = '<tr class="c-table__row"><td><input type="text" class="c-input" name="bezeichnung[]"></td><td><input type="text" class="qty c-input" name="menge[]"/></td><td><input type="text" class="price c-input" name="price[]"/></td><td colspan="2"><input type="hidden" name="total[] "class="Rowtotal"><span class="total"></span><a href="javascript:;" class="removetData"><i class="fa fa-close"></i></a></td></tr>';
+            var html = '<tr class="c-table__row"><td><input type="text" class="c-input" name="bezeichnung[]"></td><td><input type="text" class="qty c-input" name="menge[]"/></td><td><input type="text" class="price c-input" name="price[]"/></td><td colspan="2"><input type="hidden" name="total[]" class="Rowtotal"><span class="total"></span><a href="javascript:;" class="removetData"><i class="fa fa-close"></i></a></td></tr>';
             $('.dataAppend').append(html);
             
-            var finalTotal = 0;
-            $(".total").each(function(){
+           putTotal();
+           
+        });
+        function putTotal(){
+             var finalTotal = 0;
+            $(".Rowtotal").each(function(){
                 var value = $(this).val();
                 if(value != ''){
                     finalTotal += parseFloat(value);
                 }
             });
             $(".finalTotal").text(finalTotal);
-           
-        });
-        
+        }
         $("body").on("blur", ".qty" , function(){
             var qty = $(this).val();
             var price = $(this).closest('tr').find('.price').val();
@@ -57,6 +65,7 @@ var Invoice = function () {
                 $(this).closest('tr').find('.Rowtotal').val(total);
                 $(this).closest('tr').find('.total').text(total + ' €');
             }
+            putTotal();
         });
         $("body").on("click", ".deleteInvoice" , function(){
             var dataId = $(this).data('id');
@@ -78,6 +87,7 @@ var Invoice = function () {
                 $(this).closest('tr').find('.Rowtotal').val(total);
                 $(this).closest('tr').find('.total').text(total + ' €');
             }
+            putTotal();
         });
         
        
--- resources/views/admin/invoice/service-list.blade.php
+++ resources/views/admin/invoice/service-list.blade.php
@@ -9,7 +9,7 @@ $price = preg_replace("/[^0-9.]/", "", $getServiceDetail['price']);
     </td>
     <td><input type="text" class="qty c-input" value="{{ $qty }}" name="menge[]"/></td>
     <td><input type="text" class="price c-input" value="{{ $price }}" name="price[]"/></td>
-    <td><input type="hidden" name="total[] "class="Rowtotal"><span class="total">{{ $qty * $price }}</span>
+    <td><input type="hidden" name="total[]" value="{{ $qty * $price }}" class="Rowtotal"><span class="total">{{ $qty * $price }}</span>
      <a href="javascript:;" class="deleteInvoice" data-id="{{ $getServiceDetail['id'] }}"><span class="c-tooltip c-tooltip--top" data-toggle="modal" data-target="#deleteModel" aria-label="Delete">
                                         <i class="fa fa-trash-o"></i></span>
                                 </a>
