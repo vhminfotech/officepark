ebd1ac1cc8338e73c9451aa9424f0966a24af95d
abhishekdesai <abhishekdesai39@gmail.com>
2018-10-20 13:47:05 +0530

    Side Bar change languages issue

--- resources/views/layouts/include/leftpanel/admin-left-sidebar.blade.php
+++ resources/views/layouts/include/leftpanel/admin-left-sidebar.blade.php
@@ -112,7 +112,7 @@ $roles  = array_values($roles);
             <li class="c-sidebar__item">
                 <i class="fa fa-flag-icon-us"></i>
             </li>
-            <li class="c-sidebar__item" style="position: absolute; bottom: 0px; margin-bottom: 20px; padding-left: 35px;">
+            <li class="c-sidebar__item" style="position: absolute; bottom: 0px; margin-bottom: -173px; padding-left: 35px;">
                 <div class="language-selection {{ (isset($_COOKIE['language']) && ($_COOKIE['language']) == 'gr' ? 'active' : '') }}" style="display: inline;">
                     <a href="javascript:;" class="language" data-lang="gr">
                         @if((isset($_COOKIE['language'])))
