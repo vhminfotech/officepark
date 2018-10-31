84ac3bf17575eaed713c35e2e535253a3d567dba
abhishekdesai <abhishekdesai39@gmail.com>
2018-10-31 20:27:28 +0530

    solve lang issu

---
+++ resources/lang/gr/words.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    'test'     => 'test-gr',
+    'Dashboard'     => 'Instrumententafel',
+    'Overview'      => 'Überblick',
+    'Manage user'   => 'Benutzer verwalten',
+    'System-users'  => 'Systembenutzer',
+    'Customer'      => 'Kunde',
+    'Addressbook'   => 'Adressbuch',
+    'Order'         => 'Auftrag',
+    'Select'        => 'Wählen',
+    'Calls'         => 'Calls',
+    'Logout'        => 'Ausloggen',
+    'Service'       => 'OP Services',
+    
+];
---
+++ resources/lang/tr/words.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    'test'     => 'test-tr',
+    'Dashboard'     => 'gösterge paneli',
+    'Overview'      => 'genel bakış',
+    'Manage user'   => 'Kullanıcıyı yönet',
+    'System-users'  => 'Sistem kullanıcıları',
+    'Customer'      => 'müşteri',
+    'Addressbook'   => 'Adres defteri',
+    'Order'         => 'Sipariş',
+    'Select'        => 'seçmek',
+    'Calls'         => 'Aramalar',
+    'Logout'        => 'Çıkış Yap',
+    'Service' => 'OP Services',
+    
+];
