f29c6ec211d82898e806effca0681b2140f76d7f
kartik <kartikdesai123@gmail.com>
2018-11-26 23:17:25 +0530

    solved issue

--- app/Model/Employee.php
+++ app/Model/Employee.php
@@ -157,6 +157,20 @@ class Employee extends Model {
         
               
     }
+    public function employeinfo($request=null) {
+       
+        return DB::table('employee')
+                ->join('users', 'users.id', '=', 'employee.customer_id')
+                ->join('employee_details', 'employee_details.employee_id', '=', 'employee.id')
+                ->groupBy('users.customer_number')
+                ->get(['employee.*',
+                            'employee_details.day_name',
+                            'employee_details.day_start_time',
+                            'employee_details.day_end_time',
+                            'users.customer_number']);
+        
+              
+    }
 
     public function geteEmployeeEdit($id) {
         
