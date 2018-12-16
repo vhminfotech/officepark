954c81291c39fe6635f835a440de3b6dc3821879
kartik <kartikdesai123@gmail.com>
2018-12-16 11:04:10 +0530

    SOlved isssue

--- app/Model/Sendmail.php
+++ app/Model/Sendmail.php
@@ -38,7 +38,7 @@ class Sendmail extends Model {
        try {
         $pathToFile = $mailData['attachment'];
         Mail::send($mailData['template'], ['data' => $mailData['data']], function ($m) use ($mailData,$pathToFile) {
-            $m->from('kartikdesai123@gmail.com', 'Office Park');
+            $m->from('smtp@prasadexpo.co.in', 'Office Park');
 
 //            $m->to('abhishekdesai39@gmail.com', "Office Park")->subject($mailData['subject']);
             $m->to($mailData['mailto'], "Office Park")->subject($mailData['subject']);
